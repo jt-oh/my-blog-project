f0a1cb9e6ba7ed8de6946548bad0d9398f38b873
JeongTaek Oh <tei1234tei@gmail.com>
2022-11-30 22:15:16 +0900

    define persistency outbound port interface

--- src/main/java/com/my_blog/demo/post/application/outbound_ports/Posts.java
+++ src/main/java/com/my_blog/demo/post/application/outbound_ports/Posts.java
@@ -1,3 +1,14 @@
 package com.my_blog.demo.post.application.outbound_ports;
 
-public interface Posts {}
+import java.util.List;
+import java.util.Optional;
+
+import com.my_blog.demo.post.domain.Post;
+import com.my_blog.demo.post.domain.value_objects.PostId;
+
+public interface Posts {
+    public Post save(Post post);
+    public Optional<Post> find(PostId postId);
+    public List<Post> findAll();
+    public PostId deleteById(PostId postId);
+}
